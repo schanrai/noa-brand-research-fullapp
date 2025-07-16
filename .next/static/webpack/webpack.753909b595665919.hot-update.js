fcd463942271bd2f04117388d2b1ad974f5613ac
Sushi Chanrai <sushi@pathworks.co>
2025-07-16 18:25:11 -0400

    Fix brand research company name parsing
    - Updated MainContentProps interface to accept value parameter
    - Fixed onChatResponse to pass search query through to handleChatResponse
    - Ensured company name from AI co-pilot search is properly displayed in brand cards
    - Removed duplicate src/app/page.tsx file to avoid conflicts

--- .next/static/webpack/webpack.753909b595665919.hot-update.js
+++ .next/static/webpack/webpack.753909b595665919.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("a8e86f34d1f3e32c")
+/******/ 	__webpack_require__.h = () => ("6f1968068b460a6c")
 /******/ })();
 /******/ 
 /******/ }
