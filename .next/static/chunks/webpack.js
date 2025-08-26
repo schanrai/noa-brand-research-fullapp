f20c5d5909a1a40958f32d14acce2db6c94ad534
Sushi Chanrai <sushi@pathworks.co>
2025-08-26 12:34:58 -0400

    company overview render markdown

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("41a8d7727bf448ae")
+/******/ 		__webpack_require__.h = () => ("e802cb7c69938180")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
